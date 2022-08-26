53cd7a9a714e1b45a6d23f2bdaec9dda28001b7f
Shreyash Chandra <puttashreyash@gmail.com>
2022-08-26 11:40:49 +0530

    Add files via upload

---
+++ IDE assignment/code/main.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+//Declaring all variables as integers
+int D0=0,D1=0,D2=0,D3=0;
+int Q1,Q0;
+//Code released under GNU GPL.  Free to use for anything.
+void disp_encoder(int Q1, int Q0)
+{
+  digitalWrite(13, Q1); //LSB
+  digitalWrite(4, Q0); //MSB
+  
+    
+}
+// the setup function runs once when you press reset or power the board
+void setup() {
+    pinMode(4, OUTPUT);  
+    pinMode(13, OUTPUT);
+    pinMode(8, INPUT);
+    pinMode(5, INPUT);
+    pinMode(6, INPUT);
+    pinMode(7, INPUT); 
+    
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    D1 = digitalRead(5);
+    D2 = digitalRead(6);
+    D3 = digitalRead(7);
+    D0 = digitalRead(8);
+    Q1= D1 || D3; 
+    Q0= D2 || D3; 
+    disp_encoder(Q1,Q0);   
+
+}
