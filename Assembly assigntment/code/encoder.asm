4353160ccb65922b406783b86269381faca68fa0
Shreyash Chandra <puttashreyash@gmail.com>
2022-09-29 18:43:51 +0530

    Update encoder.asm

--- Assembly assigntment/code/encoder.asm
+++ Assembly assigntment/code/encoder.asm
@@ -26,21 +26,35 @@ ldi r20,0b00000000   ;D3
 
 mov r22,r18
 or r22,r20 ; Q0 = d1 || d3
+ldi r25,0x9c
+;;;;;loop1;;;;;;;
+clr r25
+l1:
+cpi r25,5   ; comparing r25 to 5
+breq next   ; break and goto next
 lsl r22
-lsl r22
-lsl r22
-lsl r22
-lsl r22
+inc r25
+rjmp l1
+;;;;;;loop1;;;;;;;;;;;;
+next:
 out PortB,r22 ; PB5
 
 ;for Q1 (R23)
 
 mov r23,r19
 or r23,r20 ; Q1 = d2 || d3
+
+clr r25  ; r25 == 0
+;;;;;;;loop2;;;;;
+
+l2:
+cpi r25,4
+breq next1   ; break and goto next1
 lsl r23
-lsl r23
-lsl r23
-lsl r23
+inc r25
+rjmp l2     
+;;;;;;;loop2;;;;;;;
+next1:
 out PortD,r23 ; PD4
 
 ;output on PORTB
